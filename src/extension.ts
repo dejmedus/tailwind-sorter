886fb4bb1d4fd2a06ec705bb7fd337350e4e9320
Julia <email96123@gmail.com>
2024-01-25 12:31:04 -0700

    Refactor tailwind sorting logic

--- src/extension.ts
+++ src/extension.ts
@@ -7,28 +7,15 @@ import sortTailwind from "./sortTailwind";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "tailwind-sorter.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from tailwind-sorter!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
   let sortDisposable = vscode.workspace.onWillSaveTextDocument((event) => {
-    const languages = ["html", "vue", "jsx", "tsx", "javascript", "typescript"];
+    console.log("language", event.document.languageId);
+    const languages = ["html", "vue", "javascriptreact", "typescriptreact"];
 
     if (languages.includes(event.document.languageId)) {
       // get current configuration settings
       const sortConfig = getClassesMap();
 
-      // get css from file and sort
+      // get tailwind from file and sort
       const text = event.document.getText();
       const sortedTailwind = sortTailwind(text, sortConfig);
 
