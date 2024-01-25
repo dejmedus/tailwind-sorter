d973ded0fb01e09d5f656f74640692e89a28f0dd
Julia <email96123@gmail.com>
2024-01-25 11:01:41 -0700

    Add getClassesMap function to retrieve properties map

---
+++ src/getClassesMap.ts
@@ -0,0 +1,51 @@
+import * as vscode from "vscode";
+import classesConfig from "./lib/classesConfig";
+
+export default function getClassesMap() {
+  // get config from workspace or use default properties and sort order
+  const config = vscode.workspace.getConfiguration("Tailwind Sorter");
+  let categories: { [category: string]: string[] } = config.get(
+    "Categories",
+    classesConfig.categories
+  );
+  let sortOrder: string[] = config.get(
+    "Sort Order",
+    classesConfig.order
+  ).sortOrder;
+
+  // ensure valid config files - categories in sort order must exist in categories list
+  let validConfig = Object.keys(categories).length === sortOrder.length;
+
+  if (validConfig) {
+    for (let i = 0; i < sortOrder.length; i++) {
+      if (!Object.keys(categories).includes(sortOrder[i])) {
+        validConfig = false;
+        console.error(
+          "CSS Style Sorter: No category ",
+          sortOrder[i],
+          " found."
+        );
+
+        break;
+      }
+    }
+  }
+
+  // if config is invalid, use defaults
+  if (!validConfig) {
+    console.error(
+      "CSS Style Sorter: Invalid configuration. Please check sort order in settings. Using default sort order."
+    );
+    categories = classesConfig.categories;
+    sortOrder = classesConfig.order.sortOrder;
+  }
+
+  let propertiesMap: { [property: string]: number } = {};
+  let index = 0;
+  for (let i = 0; i < sortOrder.length; i++) {
+    for (let j = 0; j < categories[sortOrder[i]].length; j++) {
+      propertiesMap[categories[sortOrder[i]][j]] = index++;
+    }
+  }
+  return propertiesMap;
+}
