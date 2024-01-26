cc7f98066af53d9e39bb599824104af42db729c4
Julia <email96123@gmail.com>
2024-01-25 21:24:32 -0700

    Add defaultClassesMap function for tests

--- src/getClassesMap.ts
+++ src/getClassesMap.ts
@@ -45,3 +45,17 @@ export default function getClassesMap() {
   }
   return classesMap;
 }
+
+export function defaultClassesMap() {
+  const categories: { [category: string]: string[] } = classesConfig.categories;
+  const sortOrder = classesConfig.order.sortOrder;
+
+  let classesMap: { [property: string]: number } = {};
+  let index = 0;
+  for (let i = 0; i < sortOrder.length; i++) {
+    for (let j = 0; j < categories[sortOrder[i]].length; j++) {
+      classesMap[categories[sortOrder[i]][j]] = index++;
+    }
+  }
+  return classesMap;
+}
