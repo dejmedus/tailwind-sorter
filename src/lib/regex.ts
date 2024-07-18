cc3a6225c1a202184ca61dcac02b2d22f78edbc2
Julia <email96123@gmail.com>
2024-07-17 19:48:43 -0600

    add regex test link

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -19,6 +19,7 @@ export function createRegex() {
 
   const prefixes = `(${escapedPrefixes}|class=|className=)`;
 
+  // regex101 example: https://regex101.com/r/81bCmO/1
   // (?<=\\s|^) prefix should be preceded by a space or the start of the string
   // "([^"\(<{>]*)" matches everything inside quotes group unless there is dynamic syntax inside
   const regexStr = `(?<=\\s|^)${prefixes}("([^"\(<{>]*)"|'([^'\(<{>]*)'|\`([^\`\(<{>]*)\`)`;
